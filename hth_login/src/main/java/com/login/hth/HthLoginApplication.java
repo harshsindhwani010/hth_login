dd04c680a91c4e281daa8c0f4dc4e12ebcdb8664
ABCD <akash.grover@ditstek.com>
2023-02-13 17:44:49 +0530

    tomcat prod

--- hth_login/src/main/java/com/login/hth/HthLoginApplication.java
+++ hth_login/src/main/java/com/login/hth/HthLoginApplication.java
@@ -2,9 +2,10 @@ package com.login.hth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HthLoginApplication {
+public class HthLoginApplication extends SpringBootServletInitializer {
 	public static void main(String[] args){
 		SpringApplication.run(HthLoginApplication.class, args);
 	}
--- hth_login/src/main/java/com/login/hth/security/SecurityConfiguration.java
+++ hth_login/src/main/java/com/login/hth/security/SecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .authorizeRequests()
+                .antMatchers("/*").permitAll()
                 .antMatchers("/api/v1/**")
                 .permitAll()
                 .anyRequest()
