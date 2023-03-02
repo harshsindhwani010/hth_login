e28147a4d82772d60573e17530028ddd328915c8
SouravGrover <105637432+SouravGrover@users.noreply.github.com>
2023-03-02 17:29:30 +0530

    Update ClaimsData.java

--- hth_login/src/main/java/com/login/hth/beans/ClaimsData.java
+++ hth_login/src/main/java/com/login/hth/beans/ClaimsData.java
@@ -4,7 +4,6 @@ import com.login.hth.dto.ClaimResponseDTO;
 import com.login.hth.dto.PaymentDetailDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,38 +13,37 @@ import java.util.List;
 @Component
 
 public class ClaimsData {
-    public ResponseEntity<Object> checkClaim(String ssn) {
+    public ResponseEntity<Object> checkClaim(String ssn){
         List<String[]> headerList = CLMDET.getHeaderData(ssn);
         List<ClaimResponseDTO> wholeDTOList = new ArrayList<ClaimResponseDTO>();
         List<String[]> insureList = CLMDET.getInsureData(ssn);
         String[] name = Arrays.stream(insureList.get(0)).map(String::trim).toArray(String[]::new);
         String fullName = String.join(" ", name);
-        for (int i = 0; i < headerList.size(); i++) {
+        for(int i=0;i<headerList.size();i++){
             String[] header = headerList.get(i);
 
             List<String[]> detailList = CLMDET.getDetailData(header[0]);
 
-            for (String[] detail : detailList) {
+                for(String[] detail : detailList){
 
-                double copay = Double.valueOf(detail[2].trim());
-                double notCoverd = Double.valueOf(detail[4]);
-                double deducatable = Double.valueOf(detail[5].trim());
+                    double copay       = Double.valueOf(detail[2].trim());
+                    double notCoverd   = Double.valueOf(detail[4]);;
+                    double deducatable = Double.valueOf(detail[5].trim());
 
-                PaymentDetailDTO paymentDetail = new PaymentDetailDTO();
-                paymentDetail.setTotal(header[1]);
-                paymentDetail.setPlanPaid(detail[3]);
-                paymentDetail.setPatientResponsibility(copay + notCoverd + deducatable);
-
-                ClaimResponseDTO claimResponseDTO = new ClaimResponseDTO();
-                claimResponseDTO.setClaimNumber(header[0]);
-                claimResponseDTO.setDateOfService(formattedDate(detail[1].trim()));
-                claimResponseDTO.setPatientResponsibilityDetails(copay + notCoverd + deducatable);
-                claimResponseDTO.setClaimType(header[2]);
-                claimResponseDTO.setPatientRelatipnship(header[4]);
-                claimResponseDTO.setPaymentDetails(paymentDetail);
-                claimResponseDTO.setPatient(fullName);
-                wholeDTOList.add(claimResponseDTO);
-            }
+                    PaymentDetailDTO paymentDetail = new PaymentDetailDTO();
+                    paymentDetail.setTotal(header[1]);
+                    paymentDetail.setPlanPaid(detail[3]);
+                    paymentDetail.setPatientResponsibility(copay+notCoverd+deducatable);
+                    ClaimResponseDTO claimResponseDTO = new ClaimResponseDTO();
+                    claimResponseDTO.setClaimNumber(header[0]);
+                    claimResponseDTO.setDateOfService(formattedDate(detail[1].trim()));
+                    claimResponseDTO.setPatientResponsibilityDetails(copay+notCoverd+deducatable);
+                    claimResponseDTO.setClaimType(header[2]);
+                    claimResponseDTO.setPatientRelatipnship(header[4]);
+                    claimResponseDTO.setPaymentDetails(paymentDetail);
+                    claimResponseDTO.setPatient(fullName);
+                    wholeDTOList.add(claimResponseDTO);
+                }
         }
         return ResponseEntity.ok().body(wholeDTOList);
     }
@@ -59,7 +57,7 @@ public class ClaimsData {
             if (processDate.length() == 5) {
                 processDate = "0" + processDate;
             }
-            Date d = new SimpleDateFormat("MMddyy").parse(processDate);
+            Date d = new SimpleDateFormat("MMdyyyy").parse(processDate);
             SimpleDateFormat d2 = new SimpleDateFormat("MM/dd/yyyy");
             formattedProcessDate = d2.format(d).toString();
         } catch (Exception e) {
